c9a4532cf39041def91f09a09ea5bec10fadd768
Ben Rosen <benjamin.j.rosen@gmail.com>
2020-03-23 08:53:14 -0400

    continue ligeti

--- ligeti.py
+++ ligeti.py
@@ -2,3 +2,15 @@
 
 import abjad
 from abjad.demos import ligeti
+
+pitches = [1, 2, 3]
+maker = abjad.NoteMaker()
+notes = maker(pitches, [(1, 8)])
+abjad.beam(notes)
+abjad.slur(notes)
+abjad.attach(abjad.Dynamic("f"), notes[0])
+abjad.attach(abjad.Dynamic("p"), notes[1])
+
+voice_lower = abjad.Voice(notes)
+voice_lower.name = "rh_lower"
+command = abjad.LilyPondLiteral()
